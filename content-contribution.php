485f1abf4cd80cff1be287fb30e6822c0545e60d
小城 <kojolumi@gamil.com>
2024-08-28 14:38:14 +0900

    test

--- content-contribution.php
+++ content-contribution.php
@@ -1,7 +1,12 @@
                     <article class="article-card">
                       <a class="card-link" href="<?php echo get_term_link($term); ?>">
                         <div class="image">
-                          <img alt="" src="<?php echo get_template_directory_uri(); ?>/assets/images/bg-page-dummy.png" />
+                          <!-- <img alt="" src="<?php echo get_template_directory_uri(); ?>/assets/images/bg-page-dummy.png" /> -->
+                          <?php if ( has_post_thumbnail() ) : ?>
+    <?php the_post_thumbnail(); ?>
+<?php else : ?>
+    <img alt="" src="<?php echo get_template_directory_uri(); ?>/assets/images/bg-page-dummy.png" />
+<?php endif; ?>
                         </div>
                         <div class=" body">
                           <p class="title"><?php echo $term->name; ?></p>
