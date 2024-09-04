24afc672d09b08f3ab6ccd43945996caff16fd58
小城 <kojolumi@gamil.com>
2024-09-04 14:11:36 +0900

    test

--- content-shop-detail.php
+++ content-shop-detail.php
@@ -10,11 +10,11 @@ echo wp_get_attachment_image( $image_id, 'shop-detail' );
                             <p class="shop-caption"><?php the_sub_field( 'shop_strength' ); ?></p>
                             <div class="shop-detail">
                               <dl>
-                                <dt>営業時間</dt>
+                                <dt>間取り・広さ</dt>
                                 <dd><?php the_sub_field( 'shop_hours' ); ?></dd>
                               </dl>
                               <dl>
-                                <dt>フロア情報</dt>
+                                <dt>理想・ご要望</dt>
                                 <dd><?php the_sub_field( 'floor_info' ); ?></dd>
                               </dl>
                             </div>
