15dfd965c443300ea92e5683b5eb883c9f21a290
小城 <kojolumi@gamil.com>
2024-09-06 17:21:37 +0900

    test

--- header.php
+++ header.php
@@ -62,10 +62,10 @@
             <!-- 電話番号 -->
             <a href="./" style="display: flex; flex-direction:row; box-sizing: border-box; padding: 12px 13px; border-radius: 60px; margin-top: 0px; margin-bottom: 0px; align-items: center; background-color: #fff; margin-top: 5px;
     margin-bottom: 5px;">
-              <img src="https://atrail.co.jp/wp-content/uploads/images/svg/icon-tel-circle.svg" alt="" srcset="" style="width: 35px; height: 35px;">
+              <img src="https://atrail.co.jp/wp-content/uploads/images/svg/icon-tel-circle.svg" alt="" srcset="" class="img-tel">
               <div style="display:flex; flex-direction: column; align-content: flex-start; justify-content: center; padding-left:10px">
                 <tel>0120-0000-000</tel>
-                <!-- <p style="margin-top:-10px;letter-spacing:0.1px; font-size: 12px;">受付時間 / 09:00-18:00（水曜定休日）</p> -->
+                <p class="text-tel">受付時間 / 09:00-18:00（水曜定休日）</p>
               </div>
             </a>
 
